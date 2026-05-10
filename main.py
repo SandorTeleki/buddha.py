bdd14dd34046cc3fac65bdc8673f90fd56a79712
Sandor Teleki <sandorteleki1@gmail.com>
2026-05-10 11:12:54 +0200

    fix: fixing log in

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
 import discord
 import os
+import logging
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
-CLIENT_ID = os.environ["CLIENT_ID"]
-TOKEN = os.environ["TOKEN"]
+token = os.getenv('TOKEN')
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-@client.event
+#client = discord.Client(intents=intents)
+
+#@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user.name}!')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+# #@client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+#     if message.content.startswith('$hello'):
+#         await message.channel.send('Hello!')
 
-client.run('your token here')
+bot.run(token, log_handler=handler, log_level=logging.DEBUG)
