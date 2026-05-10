1ddd58f81758340f1a8813eef9859e9c878ccfa8
Sandor Teleki <sandorteleki1@gmail.com>
2026-05-10 09:53:54 +0200

    feat: env and basic scaffolding

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # buddha.py
 Small discord bot that returns quotes attributed to Buddha.
+
+## Prerequisistes:
+- Python 3.13+
+- UV
+
+## Set-up:
--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from buddha-py!")
+import discord
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+CLIENT_ID = os.environ["CLIENT_ID"]
+TOKEN = os.environ["TOKEN"]
 
-if __name__ == "__main__":
-    main()
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('your token here')
